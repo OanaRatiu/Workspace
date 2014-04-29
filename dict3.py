f003489f488fbd8a5ae1ea3fbf3fada2a4a67eba
Oana Ratiu <oratiu@clj-pc-089.3pillar.corp>
2014-04-29 11:19:34 +0300

    final 3

--- dict3.py
+++ dict3.py
@@ -2,89 +2,79 @@ import re
 from collections import defaultdict
 
 def split_into_words(lines):
-  """
-  >>> split_into_words("a,b c!")
-  ['a', 'b', 'c']
+    """
+    >>> split_into_words("a,b c!")
+    ['a', 'b', 'c']
 
-  >>split_into_words("")
-  []
-  """
-  return re.findall(r"[\w']+", lines)
+    >>split_into_words("")
+    []
+    """
+    return re.findall(r"[\w']+", lines)
 
 
 def read_from_file(f):
-  # """
-  # >>> read_from_file(['a b c', 'a =b c!\\n'])
-  # ['a', 'b', 'c']
-  # """
-  dictio = dd()
-  words = []
-  for line in f:
-    words = split_into_words(line)
-    for word in words:
-      put_in_tree2(word, dictio)
+    dictio = dd()
+    words = []
+    for line in f:
+        words = split_into_words(line)
+        for word in words:
+            put_in_tree2(word, dictio)
+    return dictio
 
-
- #start with dicitonary that has every letter as key in it 
-def put_in_tree(word, dictio):
-  # """
-  # >>> put_in_tree('mate',{})
-  # {'m': {'a': {'t': {'e': {'times': 1}}}}}
-
-  # >> put_in_tree('mama',{})
-  # {'m': {'a': {'m': {'a': {'times': 1}}}}}
-
-  # >> put_in_tree('a',{})
-  # {'a': {'times': 1}}
-
-  # >>> put_in_tree('mates', {'m': {'a': {'t': {'e': {'times': 1}}}}})
-  # {'m': {'a': {'t': {'e': {'times': 1}, {'s': {'times': 1}}}}}}
-
-  # """
-  cd = dictio
-  for i in range(len(word)):
-    try:
-      if word[i+1] in cd[word[i]].keys():
-        cd = cd[word[i]]
-    except:
-      try:
-        if 'times' not in cd[word[i]].keys():
-          cd[word[i]] = {}
-      except:
-        cd[word[i]].update({word[i+1] : {}})
-        i = i+1
-      cd = cd[word[i]]
-
-  if cd == {}:
-    cd['times'] = 1
-  else:
-    cd['times'] += 1  
-  return dictio
 
 def dd():
-  return defaultdict(dd)
+    return defaultdict(dd)
 
 def put_in_tree2(word, dictio = dd()):
-  """
-  >>> put_in_tree2('mate') == {'m': {'a': {'t': {'e': {'times': 1}}}}}
-  True
+    """
+    >>> put_in_tree2('mate') == {'m': {'a': {'t': {'e': {'times': 1}}}}}
+    True
 
-  >>> d = dd()
-  >>> e = put_in_tree2('mate', d)
-  >>> put_in_tree2('mates', e) == {'m': {'a': {'t': {'e': {'s': {'times': 1}, 'times': 1}}}}}
-  True
+    >>> d = dd()
+    >>> e = put_in_tree2('mate', d)
+    >>> put_in_tree2('mates', e) == {'m': {'a': {'t': {'e': {'s': {'times': 1}, 'times': 1}}}}}
+    True
 
-  """
-  cd = dictio
-  for letter in word:
-    cd=cd[letter]
-  try :
-    cd['times'] = int(cd['times']) + 1
-  except TypeError:
-    cd['times'] = 1
-  return dictio
+    """
+    cd = dictio
+    for letter in word:
+        cd=cd[letter]
+    try :
+        cd['times'] = int(cd['times']) + 1
+    except TypeError:
+        cd['times'] = 1
+    return dictio
+
+
+def parse_dictionary(dictio, word = [], single_words = []):
+    """
+    >>> d = dd()
+    >>> d = {'m': {'a': {'t': {'e': {'s': {'times': 1}, 'times': 1}}}}}
+    >>> s_w = parse_dictionary(d, [], [])
+    >>> s_w == ['mates' , 'mate']
+    True
+
+    >>> s_w = parse_dictionary({'m': {'a': {'t': {'e': {'s': {'times': 1}, 'times': 1}}}}, 'z' : {'times' : 1}, 'w': {'times': 1}}, [], [])
+    >>> set(s_w) == set(['mates', 'mate', 'z' , 'w'])
+    True
+
+    >>> parse_dictionary({}, [], [])
+    []
+
+    """
+    for key in dictio:
+        if key == 'times':
+            if dictio['times'] == 1:
+                cw = ''.join(word)
+                single_words.append(cw)
+        else:
+            word.append(key)
+            parse_dictionary(dictio[key], word, single_words)
+            del word[-1]
+    return single_words
 
 
 if __name__ == '__main__':
     with open('word') as f:
-        read_from_file(f)
+        for word in parse_dictionary(read_from_file(f), [], []):
+            print word
